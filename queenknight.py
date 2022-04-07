37a0676784b6bcc3df0d376528f0ce18a63f19e3
Sanna <31210400+santras@users.noreply.github.com>
2022-04-07 12:35:00 +0300

    Shakkipulma (loppuun)
    Viikko 1, Tehtävä 7, Python versio final

--- queenknight.py
+++ queenknight.py
@@ -12,28 +12,49 @@ def ruudut(n,x,y):
         for j in range(n):
             lauta[(i,j)] = 1
     
-    print('summa alku ',sum(lauta.values()))
+    #print('summa alku ',sum(lauta.values()))
 
-    # kuningatar ei saa olla samalla rivillä tai sarakkeessa kun heppa, koska kuningattaren siirrot (1)
+    # kuningatar ei saa olla samalla rivillä tai sarakkeessa kun heppa, koska kuningattaren siirrot 
     for ind in range(n):
         lauta[(x,ind)] = 0      # rivi
         lauta[(ind,y)] = 0      # sarake
+    
+    for ind in range(1,n):      # Kulmat
+        #print(x+ind,y+ind)
+        if (x+ind,y+ind) in lauta.keys():
+            lauta[(x+ind,y+ind)] = 0
+        #print(x+ind,y-ind)
+        if (x+ind,y-ind) in lauta.keys():
+            lauta[(x+ind,y-ind)] = 0
+        #print(x-ind, y+ind)
+        if (x-ind, y+ind) in lauta.keys():
+            lauta[(x-ind, y+ind)] = 0
+        #print(x-ind,y-ind)
+        if (x-ind,y-ind) in lauta.keys():
+            lauta[(x-ind,y-ind)] = 0
 
-    print('summa välissä ', sum(lauta.values()))
+        
+      
+
+   # print('summa välissä ', sum(lauta.values()))
 
     # TÄSSÄ JOKIN VIRHE, EI MUUTA MITÄÄN 0:ksi
     #käydään läpi mahdolliset paikat
     for i in range(n):
         for j in range(n):
             if lauta[(i,j)] == 1:     # ei ole jo määritelty huonoksi
+                #print(i,j)
                 if abs(i-x)<3 and abs(j-y)<3:         #Hepan keskittämälle alueelle 5x5 ei mahdu kuningatar, eli ero ei saa olla alle 3
                     #print(i,j)
                     lauta[(i,j)] = 0
-                elif abs(x+i) == abs(y+j):            # kulmittain heppaan olevat eivät sovi koska kuningattaren viistosiirto
-                    #print(x,y,i,j)
-                    lauta[(i,j)] = 0
+                #else:
+                #for ind in range(3,n):
+                #        if abs(x+ind) == abs(y+ind):            # kulmittain heppaan olevat eivät sovi koska kuningattaren viistosiirto 
+                #            lauta[(i,j)] = 0
+                 #           print(x,y,i,j,ind,x+ind,y+ind)
 
-    print('summa loppu ', sum(lauta.values()))
+    #print(lauta)
+    #print('summa loppu ', sum(lauta.values()))
     return sum(lauta.values())
 
 
@@ -53,6 +74,6 @@ def count(n):
 
 
 if __name__ == "__main__":
-    #print(count(3)) # 0
+    print(count(3)) # 0
     print(count(4)) # 40
-    #print(count(5)) # 184
+    print(count(5)) # 184
